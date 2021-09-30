130ee78c530ba81df0069bc542a1d2157c29e286
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2021-09-30 16:35:02 +0900

    Change max-length to 120

--- health_planet.py
+++ health_planet.py
@@ -1,37 +1,40 @@
 import os
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import WebDriver
-from bs4 import BeautifulSoup
 
 
 def main() -> dict:
     logged_in_driver = login()
-    logged_on = "20210901"
-    return {logged_on: get_health_data(logged_in_driver, logged_on)}
+    created_on = "20210901"
+    return {created_on: get_health_data(logged_in_driver, created_on)}
 
 
 def login() -> WebDriver:
     driver = webdriver.Firefox()
     driver.get("https://www.healthplanet.jp/en/login.do")
     driver.find_element_by_name("loginId").send_keys(os.environ["HEALTH_PLANET_ID"])
-    driver.find_element_by_name("passwd").send_keys(
-        os.environ["HEALTH_PLANET_PASSWORD"]
-    )
+    driver.find_element_by_name("passwd").send_keys(os.environ["HEALTH_PLANET_PASSWORD"])
     driver.find_element_by_id("btnSet").click()
     return driver
 
 
-def get_health_data(logged_in_driver: WebDriver, logged_on: str) -> dict:
-    logged_in_driver.get(
-        f"https://www.healthplanet.jp/en/innerscan.do?date={logged_on}"
-    )
+def get_health_data(logged_in_driver: WebDriver, created_on: str) -> dict:
+    """Get scanned health data.
+
+    Args:
+        logged_in_driver: Logged in selenium driver.
+        created_on:
+
+    Returns:
+        dict: Health data (e.g. Weight, Body Fat, etc.)
+
+    """
+    logged_in_driver.get(f"https://www.healthplanet.jp/en/innerscan.do?date={created_on}")
     soup = BeautifulSoup(logged_in_driver.page_source, "html.parser")
     if data_table := soup.find("div", {"id": "inputdata_area"}):
-        return {
-            e.find("th").text.strip(): e.find("td").text.split()[0]
-            for e in data_table.find_all("tr")
-        }
+        return {e.find("th").text.strip(): e.find("td").text.split()[0] for e in data_table.find_all("tr")}
     else:
         return {}
 
